c0805063e9e75cc7efde24cc28823af765b9a493
Isabela Leal <isabela.leal@outlook.com>
2026-05-22 10:42:35 -0300

    feat: adiciona schema do banco de dados

---
+++ src/main/resources/schema-postgresql.sql
@@ -0,0 +1,78 @@
+CREATE TABLE IF NOT EXISTS tb_usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    senha VARCHAR(50),
+    data_criacao TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS tb_generos(
+    id_genero SERIAL PRIMARY KEY,
+    descricao VARCHAR (50)
+);
+
+CREATE TABLE IF NOT EXISTS tb_atores (
+  id_ator SERIAL PRIMARY KEY,
+  nome_ator VARCHAR (80),
+  data_nascimento DATE,
+  sexo CHAR (1)--Enum
+);
+
+CREATE TABLE IF NOT EXISTS tb_doramas (
+  id_dorama SERIAL PRIMARY KEY,
+  titulo_portugues VARCHAR (200),
+  titulo_nativo VARCHAR (200),
+  titulo_ingles VARCHAR (200),
+  sinopse VARCHAR (1000),
+  status_dorama VARCHAR(20), --Enum 
+  pais CHAR(2), --Enum
+  data_estreia DATE,
+  data_final DATE,
+  emissora_original VARCHAR(50),
+  poster VARCHAR(500) --para link ou imagem
+);
+
+CREATE TABLE IF NOT EXISTS tb_listas (
+  id_lista SERIAL PRIMARY KEY,
+  id_usuario INTEGER,
+  nome_lista VARCHAR(100),
+  descricao VARCHAR(400),
+  criada_em TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (id_usuario) REFERENCES tb_usuarios(id_usuario)
+);
+
+--AUXILIARES:
+
+CREATE TABLE IF NOT EXISTS tb_elenco (
+  id_dorama INTEGER,
+  id_ator INTEGER,
+  personagem VARCHAR(50),
+  PRIMARY KEY (id_dorama, id_ator),
+  FOREIGN KEY (id_dorama) REFERENCES tb_doramas(id_dorama),
+  FOREIGN KEY (id_ator) REFERENCES tb_atores(id_ator)
+);
+
+CREATE TABLE IF NOT EXISTS tb_doramas_generos (
+  id_dorama INTEGER,
+  id_genero INTEGER,
+  PRIMARY KEY (id_dorama, id_genero),
+  FOREIGN KEY (id_dorama) REFERENCES tb_doramas(id_dorama),
+  FOREIGN KEY (id_genero) REFERENCES tb_generos(id_genero)
+);
+
+CREATE TABLE IF NOT EXISTS tb_dias_exibicao (
+  id_dorama INTEGER,
+  dia_semana VARCHAR(15), --Enum
+  PRIMARY KEY (id_dorama, dia_semana),
+  FOREIGN KEY (id_dorama) REFERENCES tb_doramas(id_dorama)
+);
+
+CREATE TABLE IF NOT EXISTS tb_lista_doramas(
+  id_lista INTEGER,
+  id_dorama INTEGER,
+  status_visualizacao VARCHAR(20), --Enum
+  PRIMARY KEY (id_lista, id_dorama),
+  FOREIGN KEY (id_dorama) REFERENCES tb_doramas(id_dorama),
+  FOREIGN KEY (id_lista) REFERENCES tb_listas(id_lista)
+);
+
